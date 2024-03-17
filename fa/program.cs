1e820e1311052700500039d0428cb6c6c439743d
AlexanderKlyuev <161642862+AlexanderKlyuev@users.noreply.github.com>
2024-03-17 09:04:07 +0300

    Клюев Александр 3821Б1Пипр

--- fa/program.cs
+++ fa/program.cs
@@ -4,74 +4,166 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test
+namespace fans
 {
-    internal class Program
+    public class State
     {
-        public class State
-        {
-            public string Name;
-            public Dictionary<char, State> Transitions;
-            public bool IsAcceptState;
-        }
+        public string Name;
+        public Dictionary<char, State> Transitions;
+        public bool IsAcceptState;
+    }
 
-        public class FA
+    public class FA
+    {
+        protected bool? Run(State InitialState, IEnumerable<char> s)
         {
-            protected bool? Run(State InitialState, IEnumerable<char> s)
+            State current = InitialState;
+            foreach (var c in s)
             {
-                State current = InitialState;
-                foreach (var c in s)
-                {
-                    if (!current.Transitions.ContainsKey(c))
-                        return false;
-                    current = current.Transitions[c];
-                }
-                return current.IsAcceptState;
+                if (!current.Transitions.ContainsKey(c))
+                    return false;
+                current = current.Transitions[c];
             }
+            return current.IsAcceptState;
         }
+    }
 
-        public class FA3 : FA
+    public class FA1 : FA
+    {
+        public static State a = new State()
         {
-            public static State a = new State()
-            {
-                Name = "a",
-                IsAcceptState = false,
-                Transitions = new Dictionary<char, State>()
-            };
-            public State b = new State()
-            {
-                Name = "b",
-                IsAcceptState = false,
-                Transitions = new Dictionary<char, State>()
-            };
-            public State c = new State()
-            {
-                Name = "c",
-                IsAcceptState = true,
-                Transitions = new Dictionary<char, State>()
-            };
+            Name = "a",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State b = new State()
+        {
+            Name = "b",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State c = new State()
+        {
+            Name = "c",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State d = new State()
+        {
+            Name = "d",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
 
-            State InitialState = a;
+        State InitialState = a;
 
-            public FA3()
-            {
-                a.Transitions['0'] = a;
-                a.Transitions['1'] = b;
-                b.Transitions['0'] = a;
-                b.Transitions['1'] = c;
-                c.Transitions['0'] = c;
-                c.Transitions['1'] = c;
-            }
-
-            public bool? Run(IEnumerable<char> s) => base.Run(InitialState, s);
+        public FA1()
+        {
+            a.Transitions['0'] = b;
+            a.Transitions['1'] = c;
+            b.Transitions['1'] = d;
+            c.Transitions['0'] = d;
+            c.Transitions['1'] = c;
+            d.Transitions['1'] = d;
         }
 
+        public bool? Run(IEnumerable<char> s) => base.Run(InitialState, s);
+    }
+
+    public class FA2 : FA
+    {
+        public static State a = new State()
+        {
+            Name = "a",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State b = new State()
+        {
+            Name = "b",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State c = new State()
+        {
+            Name = "c",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State d = new State()
+        {
+            Name = "d",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState = a;
+
+        public FA2()
+        {
+            a.Transitions['0'] = b;
+            a.Transitions['1'] = c;
+            b.Transitions['0'] = a;
+            b.Transitions['1'] = d;
+            c.Transitions['0'] = d;
+            c.Transitions['1'] = a;
+            d.Transitions['0'] = c;
+            d.Transitions['1'] = b;
+        }
+
+        public bool? Run(IEnumerable<char> s) => base.Run(InitialState, s);
+    }
+
+    public class FA3 : FA
+    {
+        public static State a = new State()
+        {
+            Name = "a",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State b = new State()
+        {
+            Name = "b",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public State c = new State()
+        {
+            Name = "c",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState = a;
+
+        public FA3()
+        {
+            a.Transitions['0'] = a;
+            a.Transitions['1'] = b;
+            b.Transitions['0'] = a;
+            b.Transitions['1'] = c;
+            c.Transitions['0'] = c;
+            c.Transitions['1'] = c;
+        }
+
+        public bool? Run(IEnumerable<char> s) => base.Run(InitialState, s);
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
-            String s = "00110011";
-            FA3 fa3 = new FA3();
-            bool? result1 = fa3.Run(s);
+            String s = "01111";
+            FA1 fa1 = new FA1();
+            bool? result1 = fa1.Run(s);
             Console.WriteLine(result1);
+            FA2 fa2 = new FA2();
+            bool? result2 = fa2.Run(s);
+            Console.WriteLine(result2);
+            FA3 fa3 = new FA3();
+            bool? result3 = fa3.Run(s);
+            Console.WriteLine(result3);
         }
     }
 }
